d7ca4f589290924455652a5c98e4d12e8ca491f7
Valicsek David <entriek@gmail.com>
2018-10-05 00:34:56 +0200

    Install cors to be able to call requests from browser

--- server/index.js
+++ server/index.js
@@ -4,7 +4,11 @@ const path = require('path');
 const routes = require('./routes');
 const config = require('../config');
 
-app.use('*', routes);
+/** No 'Access-Control-Allow-Origin' header is present on the requested resource error */
+app.use(cors({
+  origin: `http://localhost:${config.client.port}`,
+  credentials: true
+}));
 
 app.listen(config.server.port, (req, res) => {
   console.log(`Server is listening on ${config.server.port} port`)
