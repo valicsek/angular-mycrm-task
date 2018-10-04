1100f1eebe84e4a3a1123da62b92f3da064eb6bb
Valicsek David <entriek@gmail.com>
2018-10-04 18:10:53 +0200

    Add multiple markers for testing

--- client/src/app/dashboard/dashboard.component.ts
+++ client/src/app/dashboard/dashboard.component.ts
@@ -6,14 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  latitude = 0;
-  longitude = 0;
+  latitude = 12;
+  longitude = 23;
+  markers = [];
 
   constructor() { }
 
   ngOnInit() {
-    this.latitude = 12;
-    this.longitude = 23;
+    this.markers.push({
+      longitude: 24,
+      latitude: 14
+    })
+    this.markers.push({
+      longitude: 56,
+      latitude: 14
+    })
   }
 
 }
